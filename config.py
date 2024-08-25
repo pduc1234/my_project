e9acab30257b91d4b8da22deba95a71961001b2e
pduc1234 <78724028+pduc1234@users.noreply.github.com>
2024-08-25 15:59:53 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from discord import channel
 
 
-TOKEN = 'MTIyNjU4MjE4MzQxMDQwMTM1MA.G3-CZl.PurMrqv3H_8M0iVkdiN2JZX_byV4M00WZmWt5M'
-PREFIX = '!'
-channel = '1227271846081331210'
+TOKEN = 'YOUR-BOT-TOKEN'
+PREFIX = '!' # Change yourself
+channel = 'LOG-CHANNEL-ID'
